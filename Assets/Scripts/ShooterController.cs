b33e76645dcf84b6e14897cd4e69fb2a8564dd97
Tryliom <alexishaldy@gmail.com>
2023-01-20 17:02:47 +0100

    Add new projectile
    Use raycast to destroy targets

--- Assets/Scripts/ShooterController.cs
+++ Assets/Scripts/ShooterController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Cinemachine;
+using DigitalRuby.LightningBolt;
 using StarterAssets;
 using UnityEngine;
 using UnityEngine.Serialization;
@@ -9,8 +10,9 @@ public class ShooterController : MonoBehaviour
 {
     [FormerlySerializedAs("_shootingTarget")] [SerializeField] private Transform _targetPoint;
     [SerializeField] private Transform _shootingOrigin;
-    [SerializeField] private Projectile _projectile;
     [SerializeField] private GameObject _player;
+    [SerializeField] private GameObject _lightningObject;
+    [SerializeField] private GameObject _hitEffect;
 
     private bool _canShoot = true;
     
@@ -19,6 +21,7 @@ public class ShooterController : MonoBehaviour
 
     private CameraController _cameraController;
     private CinemachineBrain _cinemachineBrain;
+    private LightningBoltScript _lightningBoltScript;
     
     // Start is called before the first frame update
     private void Start()
@@ -27,6 +30,7 @@ public class ShooterController : MonoBehaviour
         _mainCamera = Camera.main;
         _cameraController = _player.GetComponent<CameraController>();
         _cinemachineBrain = _mainCamera.GetComponent<CinemachineBrain>();
+        _lightningBoltScript = _lightningObject.GetComponent<LightningBoltScript>();
     }
 
     // Update is called once per frame
@@ -34,13 +38,17 @@ public class ShooterController : MonoBehaviour
     {
         var targetPosition = _mainCamera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, _mainCamera.farClipPlane));
         var ray = _mainCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, _mainCamera.farClipPlane));
+        RaycastHit hit;
         
-        if (Physics.Raycast(ray, out var hit))
+        if (Physics.Raycast(ray, out hit))
         {
             targetPosition = hit.point;
         }
         
         _targetPoint.position = Vector3.Lerp(_targetPoint.position, targetPosition, 0.05f);
+        
+        _lightningBoltScript.StartPosition = _shootingOrigin.position;
+        _lightningBoltScript.EndPosition = _targetPoint.position;
 
         if (!_inputs.fire)
         {
@@ -50,14 +58,27 @@ public class ShooterController : MonoBehaviour
         if (_inputs.fire && _inputs.aim && _canShoot)
         {
             _shoot();
+            
+            if (hit.collider != null && hit.collider.CompareTag("Target"))
+            {
+                StartCoroutine(HitTarget(hit.collider.GetComponent<TargetController>()));
+            }
+            
             _canShoot = false;
             _cameraController.ToggleFireCamera(true);
-        }
+        } 
     }
 
     private void _shoot()
     {
-        var position = _shootingOrigin.position;
-        Instantiate(_projectile, position, Quaternion.LookRotation(_targetPoint.position - position));
+        _lightningBoltScript.Trigger();
+        var gameObject = Instantiate(_hitEffect, _targetPoint.position, Quaternion.identity);
+        gameObject.transform.localScale = new Vector3(0f, 0f, 0f);
+    }
+    
+    private static IEnumerator HitTarget(TargetController targetController)
+    {
+        yield return new WaitForSeconds(0.1f);
+        targetController.Toggle(false);
     }
 }
